b701cb43240e7d20a9d88b594fa9ce24f6ed6a66
unknown <ogunbanjopetet10@gmail.com>
2022-08-17 11:56:38 +0100

    deployment

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,61 +1,61 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const Tour = require('../../models/tourModels');
-const Review = require('../../models/reviewModel');
-const User = require('../../models/userModels');
+// const fs = require('fs');
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// const Tour = require('../../models/tourModels');
+// const Review = require('../../models/reviewModel');
+// const User = require('../../models/userModels');
 
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('DB connection successful!'));
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('DB connection successful!'));
 
-// READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
-const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
-const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
-);
+// // READ JSON FILE
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+// const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+// const reviews = JSON.parse(
+//   fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+// );
 
-// IMPORT DATA INTO DB
-const importData = async () => {
-  try {
-    await Tour.create(tours);
-    await User.create(users, { validateBeforeSave: false });
-    await Review.create(reviews);
-    console.log('Data successfully loaded!');
-  } catch (err) {
-    console.log(err);
-  }
-  process.exit();
-};
+// // IMPORT DATA INTO DB
+// const importData = async () => {
+//   try {
+//     await Tour.create(tours);
+//     await User.create(users, { validateBeforeSave: false });
+//     await Review.create(reviews);
+//     console.log('Data successfully loaded!');
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   process.exit();
+// };
 
-// DELETE ALL DATA FROM DB
-const deleteData = async () => {
-  try {
-    await Tour.deleteMany();
-    await User.deleteMany(users, { validateBeforeSave: false });
-    await Review.deleteMany();
-    console.log('Data successfully deleted!');
-  } catch (err) {
-    console.log(err);
-  }
-  process.exit();
-};
+// // DELETE ALL DATA FROM DB
+// const deleteData = async () => {
+//   try {
+//     await Tour.deleteMany();
+//     await User.deleteMany(users, { validateBeforeSave: false });
+//     await Review.deleteMany();
+//     console.log('Data successfully deleted!');
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   process.exit();
+// };
 
-if (process.argv[2] === '--import') {
-  importData();
-} else if (process.argv[2] === '--delete') {
-  deleteData();
-}
+// if (process.argv[2] === '--import') {
+//   importData();
+// } else if (process.argv[2] === '--delete') {
+//   deleteData();
+// }
